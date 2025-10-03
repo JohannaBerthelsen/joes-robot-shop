dd711f024997b8fd40484f4b0fd32986dce5d697
Johanna Berthelsen <jber@jyskebank.dk>
2025-10-03 11:25:06 +0200

    .

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css'],
   standalone: true,
 })
-export class CatalogComponent {}
+export class CatalogComponent {
+  product: IProduct;
+
+  constructor() {
+    this.product = {
+      id: 2,
+      description:
+        'A friendly robot head with two eyes and a smile -- great for domestic use.',
+      name: 'Friendly Bot',
+      imageName: 'head-friendly.png',
+      category: 'Heads',
+      price: 945.0,
+      discount: 0.2,
+    };
+  }
+
+  getImageUrl(product: IProduct) {
+    return 'assets/images/robot-parts/' + product.imageName;
+  }
+}
