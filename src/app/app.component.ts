29c5a9fc2bc0a04d893cde08a99befeedcf30b2b
Johanna Berthelsen <jber@jyskebank.dk>
2025-10-03 08:38:42 +0200

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: true,
-    imports: [HomeComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [HomeComponent],
 })
-export class AppComponent {
-}
+export class AppComponent {}
