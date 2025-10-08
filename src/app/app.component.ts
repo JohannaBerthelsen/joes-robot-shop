46bdd7beda44ae8e0f29d32aa63c0939b6e46229
Johanna Berthelsen <jber@jyskebank.dk>
2025-10-08 12:56:26 +0200

    sign in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [
-    SiteHeaderComponent,
-    RouterOutlet,
-  ],
+  imports: [SiteHeaderComponent, RouterOutlet],
 })
 export class AppComponent {}
---
+++ src/app/user/sign-in/sign-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css'],
+  standalone: true,
+})
+export class SignInComponent {
+  constructor() {}
+}
