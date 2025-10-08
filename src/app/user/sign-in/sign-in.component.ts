eb1db5ff36f6e6128f6f2d8187c173726f0c5094
Johanna Berthelsen <jber@jyskebank.dk>
2025-10-08 15:17:49 +0200

    validating input

--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { IUserCredentials } from '../user.model';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../user.service';
